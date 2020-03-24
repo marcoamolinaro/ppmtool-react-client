60d0862084e69fb9be424327927571171923a6a5
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-03-24 17:42:15 -0300

    25. Project and header component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
 
 export class Dashboard extends Component {
     render() {
-        return <h1>Welcome to the Dashboard</h1>;
+        return (
+            <>
+            <h1>Welcome to the Dashboard</h1>
+            <ProjectItem/>
+            </>
+        );
     }
 }
 
