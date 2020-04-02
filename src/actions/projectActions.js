a3027a83238999e32bc1b157f9844725a1365836
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-04-02 09:31:05 -0300

    41. Handle Errors in UpdateProject.js

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
-import { createStore } from "redux";
 
 export const createProject = (project, history) => async dispatch => {
   try {
@@ -23,9 +22,13 @@ export const getProjects = () => async dispatch => {
 };
 
 export const getProject = (id, history) => async dispatch => {
-  const res = await axios.get(`http://localhost:8080/api/project/${id}`);
-  dispatch({
-    type: GET_PROJECT,
-    payload: res.data
-  });
+  try {
+    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+    dispatch({
+      type: GET_PROJECT,
+      payload: res.data
+    });
+  } catch (error) {
+    history.push("/dashboard");
+  }
 };
