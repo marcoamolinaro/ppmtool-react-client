c71186fe5baf1d116d461b48586933b95f6daafa
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-04-16 16:56:16 -0300

    69. Load ProjectTasks to the state

--- src/actions/backlogActions.js
+++ src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_BACKLOG } from "./types";
 
 export const addProjectTask = (backlog_id, project_task, history) => async (
   dispatch
@@ -18,3 +18,18 @@ export const addProjectTask = (backlog_id, project_task, history) => async (
     });
   }
 };
+
+export const getBacklog = backlog_id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/backlog/${backlog_id}`)
+    dispatch({
+      type: GET_BACKLOG,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    });   
+  }
+}
