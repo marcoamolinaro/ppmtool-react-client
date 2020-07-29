3fd5de61a0c30ee944c1018152fca76d3a545853
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-07-28 21:14:40 -0300

    103. Bug Fixes

--- src/store.js
+++ src/store.js
@@ -8,23 +8,21 @@ const middleware = [thunk];
 
 let store;
 
-if (window.navigator.userAgent.includes("Chrome")) {
-    store = createStore(
-        rootReducer, 
-        intialState, 
-        compose(
-            applyMiddleware(...middleware), 
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );
+const ReactReduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+if (window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools) {
+  store = createStore(
+    rootReducer,
+    intialState,
+    compose(applyMiddleware(...middleware), ReactReduxDevTools)
+  );
 } else {
-    store = createStore(
-        rootReducer, 
-        intialState, 
-        compose(
-            applyMiddleware(...middleware))
-    );
-    
+  store = createStore(
+    rootReducer,
+    intialState,
+    compose(applyMiddleware(...middleware))
+  );
 }
 
 export default store;
